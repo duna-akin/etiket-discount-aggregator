12b947d39a34eedab7c735b08612e37d10534f02
Tuna AKIN <tuna.takin@gmail.com>
2025-07-23 14:56:10 -0400

    add auto popup logic

--- background/background.js
+++ background/background.js
@@ -1,1 +1,19 @@
-// Future Service Worker for notifications
+console.log("Background script started!"); // test
+
+// import database
+import { couponDatabase } from "../data/coupon-database.js";
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+    console.log("Tab updated event fired"); // test
+    console.log("tab.url:", tab.url); // test'
+
+    // get url and domain from tab
+    const url = new URL(tab.url)
+    const domain = url.hostname.replace("www.", "");
+    
+    // check if domain is supported and show popup if so
+    if (couponDatabase[domain]) {
+        chrome.action.openPopup();
+    }
+});
