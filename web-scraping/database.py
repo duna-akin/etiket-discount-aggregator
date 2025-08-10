37f0b41701691641df908dfffc0f44440df9b82e
Tuna AKIN <tuna.takin@gmail.com>
2025-08-09 23:12:59 -0400

    add database creation and define schema

--- web-scraping/database.py
+++ web-scraping/database.py
@@ -6,4 +6,27 @@ class CouponDatabase:
     def __init__(self):
         # go up one directory to etiket root, then into data folder
         self.db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'coupons.db')
-        self.conn = None
+        self.connection = None
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.db_path)
+        cursor = self.connection.cursor()
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS coupons (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       company_name TEXT,
+                       coupon_code TEXT
+                       expiry_date TEXT,
+                       entered_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       source_url TEXT
+                    )
+                ''')
+        
+        self.connection.commit()
+        print(f"Database ready at: {self.db_path}")
+
+if __name__ == "__main__":
+    db = CouponDatabase()
+    db.connect()
+    db.connection.close()
